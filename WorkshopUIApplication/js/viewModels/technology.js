0a98fccffb3267b2a46ad30d2fd2bbd4d3e3cabd
alexbeca <balejandroc@gmail.com>
2017-02-10 22:16:20 -0600

    technology html and js added

---
+++ WorkshopUIApplication/js/viewModels/technology.js
@@ -0,0 +1,20 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * technology module
+ */
+define(['ojs/ojcore', 'knockout'
+], function (oj, ko) {
+  /**
+   * The view model for the main content view template
+   */
+  function technologyContentViewModel() {
+    var self = this;
+  }
+  
+  return technologyContentViewModel;
+});
